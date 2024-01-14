7e9fed3bf448c4aee7d9cd0058c41056f0d4dacc
Alexander <DrA1ex@users.noreply.github.com>
2024-01-14 16:45:53 +0500

    Use unsigned int (aka uint32_t) for save interval

--- src/constants.h
+++ src/constants.h
@@ -33,6 +33,6 @@
 
 #define STORAGE_HEADER                          ((uint32_t) 0xffaabbcc)
 #define STORAGE_VERSION                         ((uint8_t) 1)
-#define STORAGE_SAVE_INTERVAL                   ((uint16_t) 60000)      // Wait before commit settings to FLASH
+#define STORAGE_SAVE_INTERVAL                   (60000u)      // Wait before commit settings to FLASH
 
 #define TIMER_GROW_AMOUNT                       (8u);
