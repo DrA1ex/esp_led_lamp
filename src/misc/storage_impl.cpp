41b44d0b1acca9dc496a9731c08abc1985e5d5d6
Alexander <DrA1ex@users.noreply.github.com>
2024-02-22 13:19:31 +0500

    Move all Storage code into header

--- src/misc/storage_impl.cpp
+++
@@ -1,7 +0,0 @@
-#include "storage.cpp"
-#include "config.h"
-
-template class Storage<Config>;
-template class Storage<PresetNames>;
-template class Storage<PresetConfigs>;
-template class Storage<CustomPaletteConfig>;
