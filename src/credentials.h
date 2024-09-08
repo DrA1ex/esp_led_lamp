23ff708d6b3836f52a1f07ed372d216dc8c15574
Alexander <DrA1ex@users.noreply.github.com>
2024-09-08 23:47:27 +0500

    Fix default credentials

--- src/credentials.h
+++ src/credentials.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define CREDENTIAL_WIFI_SSID                               "ESP_LED"
-#define CREDENTIAL_WIFI_PASSWORD                           "12345678"
+#define WIFI_SSID                               "ESP_LED"
+#define WIFI_PASSWORD                           "12345678"
 
-#define CREDENTIAL_AUTH_USER                               "esp_lamp"
-#define CREDENTIAL_AUTH_PASSWORD                           "password"
+#define AUTH_USER                               "esp_lamp"
+#define AUTH_PASSWORD                           "password"
 
-#define MQTT_HOST                                   	   "example.com"
-#define MQTT_PORT                                  		   (1234u)
-#define MQTT_USER                                    	   "esp_user"
-#define MQTT_PASSWORD                                	   "esp_pass"
+#define MQTT_HOST                               "example.com"
+#define MQTT_PORT                               (1234u)
+#define MQTT_USER                               "esp_user"
+#define MQTT_PASSWORD                           "esp_pass"
